422688d9431cba07b38f1cee1d45ff21389a1c0c
Josue Carrera <josuecarrera788@gmail.com>
2025-10-01 13:25:32 -0300

    agrego apartado de contraseña en user

--- user/models.py
+++ user/models.py
@@ -1,20 +1,30 @@
 from django.db import models
 from BoardOfDirectors.models import ConsejoDirectivo
+from django.contrib.auth.hashers import make_password, check_password
 
 class User(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     edad = models.PositiveIntegerField()
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True)  # será usado como username
+    password = models.CharField(max_length=128)  # para guardar contraseña hasheada
     
-        # Un usuario pertenece a un único consejo directivo
+    # Un usuario pertenece a un único consejo directivo
     consejo = models.ForeignKey(
         ConsejoDirectivo,
         on_delete=models.CASCADE,
         related_name='miembros',
-        null=True, blank=True  # opcional, si hay usuarios que no son parte de un consejo
+        null=True, blank=True
     )
-
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
+
+    # Método para establecer contraseña hasheada
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+        self.save()
+    
+    # Método para verificar contraseña
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,7 @@ from .views import alta_user,lista_procesos_disponibles, iniciar_proceso, llenar
 
 urlpatterns = [
     path('', lista_procesos_disponibles, name="lista_procesos_disponibles"),
-    path('alta/', alta_user, name='alta_user'),
+    path('alta/', alta_user, name='alta_user'), #login
     path('procesos/', lista_procesos_disponibles, name="lista_procesos_disponibles"),
     path('procesos/iniciar/<int:proceso_id>/', iniciar_proceso, name='iniciar_proceso'),
     path('procesos/llenar_datos/', llenar_datos_proceso, name='llenar_datos_proceso')
